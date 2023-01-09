fab02e49bdb0650935814e556ac05b50ce733fa5
sankethtj <sankethjaganath@gmail.com>
2023-01-09 22:31:12 +0530

    First Section completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import Profile from './Components/Profile';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Profile />
       
     </div>
   );
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+
+const Profile = () => {
+  return (
+    <Box w="100%" h="800px"  bgImage="url('https://images.pexels.com/photos/14827102/pexels-photo-14827102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')" >
+
+    </Box>    
+  )
+}
+
+export default Profile;
