1356786b892c33690a9715d25729d94e5176e92d
sankethtj <sankethjaganath@gmail.com>
2023-01-09 21:47:55 +0530

    Navbar Added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Flex, Image,Box, ButtonGroup, Button } from '@chakra-ui/react';
+import Sanketh from "../Images/Sanketh-navbar-logo.png";
+import styles from "../Styles/Navbar.module.css";
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.navbar}>
+      <Flex justifyContent="space-around" minWidth="max-content" alignItems="center" gap="2">
+        <Box>
+          <Image src={Sanketh} alt='Sanketh-Logo' />
+        </Box>
+        <ButtonGroup gap = "4">
+          <Button colorScheme="white" bgColor="#f0474a" >Home</Button>
+          <Button colorScheme="white" bgColor="#f0474a">About Me</Button>
+          <Button colorScheme="white" bgColor="#f0474a">Skills</Button>
+          <Button colorScheme="white" bgColor="#f0474a">Projects</Button>
+          <Button colorScheme="white" bgColor="#f0474a">Contact</Button>
+          <Button colorScheme="white" bgColor="#f0474a">Resume</Button>
+        </ButtonGroup>
+      </Flex>
+      
+    </div>
   )
 }
 
